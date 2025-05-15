9894b51db7ae266c5c780d31f5bd55d2c391ac8a
Sergio Galdos <galdos.sergio@outlook.com>
2025-05-15 23:12:39 +0200

    [DEV] ADD exer1

--- IntroduccionDeDatos/exer1.mjs
+++ IntroduccionDeDatos/exer1.mjs
@@ -1,3 +1,26 @@
 /////////// EXERCISE 1 ///////////
 
 "https://docs.google.com/document/d/1Mxt5M6GjlEIWzs81cgeSoVvwt_yU1FrXDcF3eF52F3I/edit?tab=t.0"
+
+/////////// EXERCISE 1 ///////////
+import readline from 'node:readline';
+
+main()
+
+function main() {
+  dataPrompt()
+}
+
+function dataPrompt() {
+  const question = "Insert your character name: ";
+  
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  rl.question(question, (name) => {
+    console.log(`Your character name is: ${name}`)
+    rl.close()
+  })
+}
